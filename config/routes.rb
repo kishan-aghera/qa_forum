5cd08cba26f80eabd642d732b21a97ad28890f6e
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2023-01-01 12:15:13 +0530

    feat: Added ability to sign in with either username or email.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'page#home'
   devise_for :users
-  get 'page/change'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
+  get 'page/change'
+  root 'page#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   # Defines the root path route ("/")
   # root "articles#index"
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
